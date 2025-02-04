a3cd6027341b383c3eec10ceb540a15691c090e9
Dimitri <kenwo.dm@gmail.com>
2025-02-04 07:40:05 +0400

    full create leave type validator

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using MediatR;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
@@ -17,6 +18,11 @@ public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeComm
     public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
     {
         // Fluent Validation
+        var validator = new CreateLeaveTypeCommandValidator(_leaveTypeRepository);
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (validationResult.Errors.Any())
+            throw new BadRequestException("Invalid LeaveType", validationResult);
 
         // Convert to domain entity obj
         var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class CreateLeaveTypeCommandValidator : AbstractValidator<CreateLeaveTypeCommand>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public CreateLeaveTypeCommandValidator(ILeaveTypeRepository leaveTypeRepository)
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("{PropertyName} is required")
+            .NotNull()
+            .MaximumLength(50).WithMessage("{PropertyName} must be fewer than 50 characters");
+
+        RuleFor(p => p.DefaultDays)
+            .GreaterThan(100).WithMessage("{PropertyName} can not exceed 100")
+            .LessThan(1).WithMessage("{PropertyName} can not be less than 1");
+
+        RuleFor(p => p)
+            .MustAsync(LeaveTypeNameUnique)
+            .WithMessage("Leave type already exists");
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+
+    private Task<bool> LeaveTypeNameUnique(CreateLeaveTypeCommand command, CancellationToken token)
+    {
+        return _leaveTypeRepository.IsLeaveTypeUnique(command.Name);
+    }
+}
